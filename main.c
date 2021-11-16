bbb088622cb9bfaef5a0f4ea78925d9e280b050d
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-11-15 22:06:38 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
+/**********************************************************************************************************
+* Developer(s): Tennison Hoffman, Saleem Griggs-Taylor, Kevin Figurski
+* Start Date: 11/15/21
+* Last modified: 11/15/21
+* Description: This code was developed to interface and model the MSP432 full capabilities by programming
+* a control system that will operate an LCD screen, motors, keypad, and LEDs.
+***********************************************************************************************************/
 #include "Functions_Lib.h"
 #include "msp.h"
 #include <stdio.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <stdint.h>
