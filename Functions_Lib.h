dd2a29157ba28e47136f7768b15e5026e8135e55
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-11-15 22:16:20 -0500

    Update Functions_Lib.h

--- Functions_Lib.h
+++ Functions_Lib.h
@@ -3,3 +3,4 @@ char Door_Feedback_Function(char Door_State);
 void Door_Function();
 char EStop_Function(char EStop_State);
 char Keypad_Function(char Num_Pressed);
+void LCD_Brightness(char Brightness_Level);
