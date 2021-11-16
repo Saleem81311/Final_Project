e1c957965b21452db6d0745cbb79f8bf034a5b91
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-11-15 22:23:17 -0500

    Update Door_Feedback_Function.c

--- Door_Feedback_Function.c
+++ Door_Feedback_Function.c
@@ -1,5 +1,5 @@
 #include "Functions_Lib.h"
-char Door_Feedback_Function(char Door_State)    // This function is made to operate the door of the......
+char Door_Feedback_Function(char Door_State)    // This function sends feedback on the current state of the door
 {
   
 }
