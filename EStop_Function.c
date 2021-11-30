f9c17f872883b6dae91aa82d13ec96cd78101651
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-11-30 18:03:24 -0500

    Update EStop_Function.c

--- EStop_Function.c
+++ EStop_Function.c
@@ -1,35 +1,36 @@
 #include "Functions_Lib.h"
-char EStop_Function(char EStop_State)   // This is the EStop function to stop everything in case of an emergency
-{
-void Pin_Init();        // Custom function defining
-void SysTick_Init();        // Custom function defining
-void SysTick_Delay(uint16_t delay);     // Custom function defining
+#include "msp.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#define Estop 0x40     // Estop button BIT on Port 1
+#define Reset 0x80     // Reset button BIT on Port 1
 
+char EStop_Function(char Operation_State)   // This is the EStop function to stop everything in case of an emergency
+{
+  void Pin_Init();        // Custom function defining
+  void SysTick_Init();        // Custom function defining
+  void SysTick_Delay(uint16_t delay);     // Custom function defining
+
+volatile char Operation_State;
+volatile int count1;     // Beginning the system count down value at 3 for when the system goes into the count down state
 void main()
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
 
-    char Operation_State = 'A';     // Initializing the first start of the system to be in the Active state
-    int count1 = 3;     // Beginning the system count down value at 3 for when the system goes into the count down state
-    int count2 = 0;
+    Operation_State = 'A';     // Initializing the first start of the system to be in the Active state
+
     int i;
     Pin_Init();     // Calling the pin initializations
     SysTick_Init();     // Calling the SysTick initializations
+    count1 = 3;
+
+    NVIC->ISER[1]=1<<((PORT1_IRQn) & 31);
+    __enable_interrupt();
 
     while(1)
     {
-        if(!(P1->IN & BIT6))        // Checking to see if the Estop is pressed
-          {
-            Operation_State = 'E';      // Sending the state of the button to ther Estop state
-            count2 = 1;     // Setting the count2 to 1 for going into the count state before the system resumes operation
-          }
-        else
-          {
-            if(count2 == 1)     // If the count2 value is 1 it sends the current state to the count
-              Operation_State = 'C';
-          }
-         SysTick_Delay(50);     // Estop Button debouncing
-
         switch(Operation_State)     // System operation state
         {
           case 'A':     // System Active State
@@ -58,7 +59,6 @@ void main()
                    count1--;        // Counting down
                    SysTick_Delay(1000);     // Delay time for the count down in ms
                  }
-              count2 = 0;       // Setting count2 back to 0 for the next time the Estop is pressed to come back into the count down state first
               count1 = 3;       // Resetting the count timer for the system in the count down state
               P2->OUT &= ~0x38;     // Turning off all LEDs on their respective BIT locations
               Operation_State = 'A';        // Setting the system back into active operation
@@ -71,18 +71,37 @@ void main()
 
 void Pin_Init()     // Pin initializations
 {
-/*********************************************************** LEDs mimicking a running system ***********************************************************/
+/**************************************************************** LEDs mimicking a running system ****************************************************************/
     P2->SEL0 &= ~0x38;
     P2->SEL1 &= ~0x38;
     P2->DIR |= 0x38;
     P2->OUT &= ~0x38;
 
-/********************************************************************* Estop Button ********************************************************************/
-    P1->SEL0 &= ~BIT6;
-    P1->SEL1 &= ~BIT6;
-    P1->DIR &= BIT6;
-    P1->REN |= BIT6;
-    P1->OUT |= BIT6;
+/********************************************************************* Estop and Reset Button ********************************************************************/
+    P1->SEL0 &= ~(Estop|Reset);
+    P1->SEL1 &= ~(Estop|Reset);
+    P1->DIR &= ~(Estop|Reset);
+    P1->REN |= (Estop|Reset);
+    P1->OUT |= (Estop|Reset);
+    P1->IES |= (Estop|Reset);
+    P1->IE  |= (Estop|Reset);
+    P1->IFG = 0;
+}
+
+void PORT1_IRQHandler()
+{
+    if(P1->IFG & Estop)     // If statement for looking for the Estop interrupt then sending it to Estop state
+    {
+        Operation_State = 'E';
+    }
+    SysTick_Delay(100);
+
+    if(P1->IFG & Reset)     // If statement for looking for the Reset interrupt then sending it to count down state
+    {
+        Operation_State = 'C';
+    }
+    SysTick_Delay(100);
+    P1->IFG = 0;
 }
 
 void SysTick_Init()     // SysTick Initializations
@@ -99,4 +118,5 @@ void SysTick_Delay(uint16_t delay)      // SysTick calculations for the delay ti
     SysTick->VAL = 5;
     while((SysTick->CTRL & 0x00010000) == 0);
 }
+
 }
