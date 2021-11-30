29480275744f35b4537171827a0a81ca07c80843
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-11-30 18:06:06 -0500

    Update Functions_Lib.h

--- Functions_Lib.h
+++ Functions_Lib.h
@@ -1,7 +1,7 @@
 int DC_Motor_Function(int PWM);
 char Door_Feedback_Function(char Door_State);
 void Door_Function();
-char EStop_Function(char EStop_State);
+char EStop_Function(char Operation_State);
 char Keypad_Function(char Num_Pressed);
 void LCD_Brightness(char Brightness_Level);
 void LCD_Function();
