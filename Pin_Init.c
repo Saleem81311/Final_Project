bc863a02fe6a027f822abc17b92ea8146f17537c
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-11-30 18:16:20 -0500

    Update Pin_Init.c

--- Pin_Init.c
+++ Pin_Init.c
@@ -1,1 +1,18 @@
+void Pin_Init()     // Pin initializations
+{
+  /*********************************************************** LEDs mimicking a running system ***********************************************************/
+    P2->SEL0 &= ~0x38;
+    P2->SEL1 &= ~0x38;
+    P2->DIR |= 0x38;
+    P2->OUT &= ~0x38;
 
+/********************************************************************* Estop Button ********************************************************************/
+    P1->SEL0 &= ~(Estop|Reset);
+    P1->SEL1 &= ~(Estop|Reset);
+    P1->DIR &= ~(Estop|Reset);
+    P1->REN |= (Estop|Reset);
+    P1->OUT |= (Estop|Reset);
+    P1->IES |= (Estop|Reset);
+    P1->IE  |= (Estop|Reset);
+    P1->IFG = 0;
+}
