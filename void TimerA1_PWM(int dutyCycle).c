0b671bbc423bbcac88cab69a2110bf5566954e6b
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 23:34:57 -0500

    Create void TimerA1_PWM(int dutyCycle).c

---
+++ void TimerA1_PWM(int dutyCycle).c
@@ -0,0 +1,9 @@
+{               //runs timer given the specified duty cycle
+  TIMER_A1-> CTL = 0b1001010100;                      //Count up using smclk, clears TAOR register, /2
+  TIMER_A1-> CCR[0] = 37500 - 1;                      //TimerA will count up to 37500-1
+    if (duty == 0)
+        TIMER_A1-> CCR[4] = 0;
+  else
+  TIMER_A1-> CCR[4] = (37500 * dutyCycle / 100) - 1;   //Sets the duty cycle.
+  TIMER_A1-> CCTL[4] = 0b11100000;                    //reset/set mode
+}
