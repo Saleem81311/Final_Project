97aac2cc1d8e40895fa74422480876a1de6ed89a
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 23:32:26 -0500

    Create void Set_Motor_Speed (void).c

---
+++ void Set_Motor_Speed (void).c
@@ -0,0 +1,12 @@
+{
+  int num = 0, speed = 0;
+  num = Read_Keypad();                   //get key press from the keypad
+    if ((num != 10) && (num != 12) && (num > 0) && (num < 13))
+    {
+        if (num == 11)
+            num = 0;
+        speed = num * 10;
+        printf("Speed Entered: %d\n", num);    //confirms speed
+        TimerA1_PWM(speed);                        //changes timer to new PWM
+    }
+}
