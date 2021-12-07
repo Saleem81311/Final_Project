d04e9781d14a10e2d1afc8cd8d28f987a5cba2d6
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 20:52:56 -0500

    Update void LCD_Command(unsigned char command).c

--- void LCD_Command(unsigned char command).c
+++ void LCD_Command(unsigned char command).c
@@ -103,17 +103,3 @@ void LCD_Command(unsigned char command)
     else
       SysTick_Delay(1);    // Any others need 40 us
 }
-
-void Message(char Menu[])
-{
-    int i;
-
-    for(i = 0; i < strlen(Menu); i++)
-    {
-        LCD_Data(Menu[i]);
-        if(i == (strlen(Menu)-1))       // strlen = String length meaning the amount of characters in the array and subtracting it by one so that we are sure it will go in this statement due to if i equals the string length it will not execute the statement any longer
-        {
-            LCD_Command(0x0C);      // Turn cursor off after it is done printing statements
-        }
-    }
-}
