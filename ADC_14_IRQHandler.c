0b7014f630e9149fee4234da6193fd774d0825aa
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-07 18:27:15 -0500

    Update ADC_14_IRQHandler.c

--- ADC_14_IRQHandler.c
+++ ADC_14_IRQHandler.c
@@ -3,8 +3,10 @@
 
 void ADC14_IRQHandler(void)
 {
-  if (ADC14->IFGR0 & BIT0){
-  nADC = (ADC14->MEM[0] * 3.3) / 255;
-  duty = nADC / 3.3;
-  TimerA3_PWM((int)(duty * 100));
+  if (ADC14->IFGR0 & BIT0)
+  {
+    nADC = (ADC14->MEM[0] * 3.3) / 255;
+    duty = nADC / 3.3;
+    TimerA3_PWM((int)(duty * 100));
+  }
 }
