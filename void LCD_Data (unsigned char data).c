a0237149d9bbedd7006a9f042256ed8289731a2b
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 20:47:11 -0500

    Update void LCD_Data (unsigned char data).c

--- void LCD_Data (unsigned char data).c
+++ void LCD_Data (unsigned char data).c
@@ -1,1 +1,9 @@
-
+{
+  P6->OUT |= RS;      // RS = 1
+  P6->OUT &= ~RW;     // RW = 0
+  P9->OUT = data;     // Put data on bus
+  P6->OUT |= EN;      // Pulse EN
+  SysTick_Delay(1);
+  P6->OUT &= ~EN;     // Clear EN
+  SysTick_Delay(2);   // Wait for controller to do the display
+}
