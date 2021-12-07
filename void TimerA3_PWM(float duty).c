1d6c64cec8a5056584f3bc179d22652928b2dca1
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 22:01:16 -0500

    Update void TimerA3_PWM(float duty).c

--- void TimerA3_PWM(float duty).c
+++ void TimerA3_PWM(float duty).c
@@ -1,3 +1,9 @@
 {
-  
+  TIMER_A3-> CTL = 0b1000010100;                      //Count up using smclk, clears TAOR register, /1
+  TIMER_A3-> CCR[0] = 10000 - 1;                      //300 Hz
+    if (duty < 3)
+        TIMER_A3-> CCR[2] = 0;
+    else
+        TIMER_A3-> CCR[2] = (10000 * (duty / 100)) - 1;
+  TIMER_A3-> CCTL[2] = 0b11100000;                    //reset/set
 }
