88520b338f86e64e40cb988882b48b828a010778
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-07 00:20:27 -0500

    Update Functions_Lib.h

--- Functions_Lib.h
+++ Functions_Lib.h
@@ -1,10 +1,19 @@
-int DC_Motor_Function(int PWM);
-char Door_Feedback_Function(char Door_State);
-void Door_Function();
-char EStop_Function(char Operation_State);
-char Keypad_Function(char Num_Pressed);
-void LCD_Brightness(char Brightness_Level);
-void LCD_Function();
-void RGB_LED_Brightness(char Brightness_Level);
-char RGB_LED_EN_DIS(char LED_State);
-void Pin_Init();
+void ADC14_IRQHandler(void);
+void ADC14_init (void);
+void delay_micro(unsigned micro);
+void delay_ms(unsigned ms);
+void Door_Control (int pos);
+char EStop_Function(char Operation_State);   // This is the EStop function to stop everything in case of an emergency
+int Read_Keypad (void);      //detects and reads key pressed
+void LCD_Command(unsigned char command);
+void LCD_Data (unsigned char data);
+void LCD_Function(int Menu_Switch);
+void Message(char Menu[]);
+void Set_Motor_Speed(void);
+void PORT6_IRQHandler(void);
+void Pin_Init();     // Pin initializations
+void SysTick_Delay(uint16_t delay);
+void SysTick_Init();
+void TimerA1_PWM(int dutyCycle);
+void TimerA2_PWM(int position);
+void TimerA3_PWM(float duty);
