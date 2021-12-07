5b0b8537964303de33669289aa969a4e29fc4d67
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 23:21:23 -0500

    Create void Door_Control (int pos).c

---
+++ void Door_Control (int pos).c
@@ -0,0 +1,14 @@
+{
+  if (pos == 1)
+  {
+    P3->OUT &= ~BIT6;
+    P3->OUT |= BIT7;
+    TimerA2_PWM(1);
+  }
+  if (pos == 0)
+  {
+    P3->OUT &= ~BIT7;
+    P3->OUT |= BIT6;
+    TimerA2_PWM(0);
+  }
+}
