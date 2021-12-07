cffe8bf6711b5fcb44045e6af8126ccfbfb96e24
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 23:49:07 -0500

    Update main.c

--- main.c
+++ main.c
@@ -14,25 +14,6 @@
 #define RW 0x40        // Goes to GND since always writing to LCD
 #define EN 0x80        // P6.5 mask
 
-int Read_Keypad (void);                         //function declarations
-void TimerA1_PWM(int dutyCycle);
-void Pin_Init(void);
-void Set_Motor_Speed (void);
-void Door_Control (int pos);
-void TimerA2_PWM(int position);
-void ADC14_init (void);
-void TimerA3_PWM(float duty);
-void ADC14_IRQHandler(void);
-void SysTick_Init(void);
-void pushNibble(uint8_t nibble);
-void pulseEnablePin(void);
-void intializeDisplay(void);
-void delay_micro(unsigned micro);
-void delay_ms(unsigned ms);
-void pushByte(uint8_t byte);
-void commandWrite(uint8_t command);
-void dataWrite(uint8_t data);
-void printString(char string[]);
 
 float nADC, duty = 100;
 
