661452d300484abb80fc0c7d4eeb12d4ca46ddcd
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 23:26:44 -0500

    Update main.c

--- main.c
+++ main.c
@@ -109,36 +109,4 @@ void PORT6_IRQHandler(void){
         }
     }
 }
-
-void Door_Control (int pos){
-    if (pos == 1){
-        P3->OUT &= ~BIT6;
-        P3->OUT |= BIT7;
-        TimerA2_PWM(1);
-    }
-    if (pos == 0){
-        P3->OUT &= ~BIT7;
-        P3->OUT |= BIT6;
-        TimerA2_PWM(0);
-    }
-}
-
-void TimerA2_PWM(int position){               //runs timer given the specified duty cycle
-    TIMER_A2-> CTL = 0b1000010100;                      //Count up using smclk, clears TAOR register, /1
-    TIMER_A2-> CCR[0] = 63000 - 1;                      //TimerA will count up to 63000-1
-    if (position == 0){
-        TIMER_A2-> CCR[4] = 2700 - 1;
-        TIMER_A2-> CCR[3] = 2700 - 1;
-        TIMER_A2-> CCR[2] = 6300 - 1;
-
-    }
-    if (position == 1){
-        TIMER_A2-> CCR[4] = 6300 - 1;
-        TIMER_A2-> CCR[3] = 6300 - 1;
-        TIMER_A2-> CCR[2] = 2700 - 1;
-    }
-    TIMER_A2-> CCTL[4] = 0b11100000;
-    TIMER_A2-> CCTL[3] = 0b11100000;                    //reset/set mode
-    TIMER_A2-> CCTL[2] = 0b11100000;
-}
 }
