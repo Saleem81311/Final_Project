e39f82161b7f967b6a4478bbf9826e26f8a49242
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 20:49:21 -0500

    Create void Message(char Menu[]).c

---
+++ void Message(char Menu[]).c
@@ -0,0 +1,12 @@
+{
+    int i;
+
+    for(i = 0; i < strlen(Menu); i++)
+    {
+        LCD_Data(Menu[i]);
+        if(i == (strlen(Menu)-1))       // strlen = String length meaning the amount of characters in the array and subtracting it by one so that we are sure it will go in this statement due to if i equals the string length it will not execute the statement any longer
+        {
+            LCD_Command(0x0C);      // Turn cursor off after it is done printing statements
+        }
+    }
+}
