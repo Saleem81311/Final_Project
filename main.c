ce3ae69045e7cb19d55f7ba254d456aeec91a917
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-06 23:41:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
  * Description: Program to combine all aspects of control system together.
  *****************************************************************************************************************/
 #include "msp.h"
+#include "Functions_Lib.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <math.h>
 #define RS 0x20        // P6.4 mask
 #define RW 0x40        // Goes to GND since always writing to LCD
 #define EN 0x80        // P6.5 mask
