7862a6db3008b111180846dd1d410ec6ac516c0e
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-07 00:07:45 -0500

    Update and rename void TimerA3_PWM(float duty).c to TIMER_A3_PWM.c

--- TIMER_A3_PWM.c
+++ TIMER_A3_PWM.c
@@ -1,5 +1,6 @@
 #include "Functions_Lib.h"
 
+void TimerA3_PWM(float duty)
 {
   TIMER_A3-> CTL = 0b1000010100;                      //Count up using smclk, clears TAOR register, /1
   TIMER_A3-> CCR[0] = 10000 - 1;                      //300 Hz
