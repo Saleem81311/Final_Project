02a4ebaec7b8bce0bf01b3a8f01bc2d241c4a1bb
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-07 17:48:11 -0500

    Update LCD_Brightness.c

--- LCD_Brightness.c
+++ LCD_Brightness.c
@@ -35,48 +35,6 @@ main(void){
     }
 }
 
-void Pin_Init(void){
-    P6->SEL0 |= BIT1;                     // configure pin 6.1 for A14 input
-    P6->SEL1 |= BIT1;
-    P6->DIR  &= ~BIT1;                      //Analog INPUT
-
-    P8-> SEL0 |= BIT2;
-    P8-> SEL1 &=~ BIT2;
-    P8-> DIR |= BIT2;                                 //sets pin 8.2 as timerA3.2
-}
-
-void delaysetup (void)                  // sets up the countdown timer
-{
-    SysTick-> CTRL = 0;
-    SysTick-> LOAD = 0x00FFFFFF;
-    SysTick-> VAL    = 0;
-    SysTick-> CTRL = 0x00000005;
-}
-
-void delayms(uint16_t delay)                // timer function set to run a 1 ms increments
-{
-    SysTick->LOAD = ((delay * 3000)-1);
-    SysTick->VAL = 0;
-    while((SysTick-> CTRL & 0x00010000) == 0);
-}
-
-void ADC14_init (void)
-{
-    /*ADC14->CTL0 &=~ 0x00000002;          // disable ADC14ENC during configuration
-    ADC14->CTL0 |=   0x04400110;          // S/H pulse mode, SMCLK, 16 sample clocks
-    ADC14->CTL1 =   0x00000030;          // 14 bit resolution
-    ADC14->CTL1 |=   0;          // Selecting ADC14CSTARTADDx mem0 REGISTER
-    ADC14->MCTL[0] = 0;            // ADC14INCHx = 0 for mem[0]
-    ADC14->CTL0 |= 0x00000002;          // enable ADC14ENC, starts the ADC after configuration*/
-    ADC14->CTL0 &=~ 0x00000002;          // disable ADC14ENC during configuration
-    ADC14->CTL0 = 0b11000101111001000111011110010000;
-    ADC14->CTL1 = 000000000000000110000;
-    ADC14->MCTL[0] = 14 | BIT7;
-    ADC14->CTL0 |= BIT1;
-    ADC14->CTL0 |= BIT0;
-    ADC14->IER0 |= BIT0;
-}
-
 void TimerA3_PWM(float duty){               //runs timer given the specified duty cycle
     TIMER_A3-> CTL = 0b1000010100;                      //Count up using smclk, clears TAOR register, /1
     TIMER_A3-> CCR[0] = 10000 - 1;                      //300 Hz
