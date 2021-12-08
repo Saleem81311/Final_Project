6c004af44cc4d08ddb193343bd642726b50741c7
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-12-07 20:49:23 -0500

    Update LCD_Data.c

--- LCD_Data.c
+++ LCD_Data.c
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <stdint.h>
 #include <math.h>
+#define RS 0x20        // P6.4 mask
+#define RW 0x40        // Goes to GND since always writing to LCD
+#define EN 0x80        // P6.5 mask
 
 void LCD_Data (unsigned char data)
 {
