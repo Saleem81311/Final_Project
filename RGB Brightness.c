8c623c9dad0fbe998e2d7352706b18b347c5c6fc
imgoodthanks <93628701+imgoodthanks@users.noreply.github.com>
2021-12-08 17:55:15 -0500

    Update RGB Brightness.c

--- RGB Brightness.c
+++ RGB Brightness.c
@@ -1,198 +1,175 @@
 #include "msp.h"
-#include "Functions_Lib.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <math.h>
 
-void b1pressed(void);     //declare functions
-void SysTick_Delay(int delayms);
 void RGB_init(void);
 void TimerA0_PWM(int duty1, int duty2, int duty3);
 int Read_Keypad(void);
 int arrayToDuty(int code[]);
+void PORT1_IRQHandler(void);
 
-int dutyR, dutyG, dutyB;
-int tempR, tempG, tempB;
-int code[3];
-int i, j;
-
+int dutyR = 100, dutyG = 100, dutyB = 100;
+int tempR = 0, tempG = 0, tempB = 0;
 
 enum states{                //enum for the switch states
-    ENABLE,
-    RED_ON,
-    BLUE_ON,
-    GREEN_ON,
-    DISABLE
+    DEFAULT,
+    RED_ON, //red menu
+    BLUE_ON, //blue menu
+    GREEN_ON //green menu
 };
 
-
 void main(void){
-    int num;
-
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
 
-    RGB_init();
+    int num, i = 0;
+    int code[3];
+    enum states state = DEFAULT;
 
-    enum states state = DISABLE;        //start with LED off
+    TimerA0_PWM(dutyR,dutyG,dutyB);
+    RGB_init();
+    NVIC_EnableIRQ(PORT1_IRQn);
+    __enable_interrupts();
 
     while (1){
-
         switch(state){
-            case DISABLE:
-                tempR=dutyR;    tempG=dutyG;    tempB=dutyB;
-                dutyR=0;    dutyG=0;    dutyB=0;
-                
-                TimerA0_PWM(dutyR, dutyG, dutyB);
-                
-                if(Read_Keypad() == 1){
-                    SysTick_Delay(1000000);
-                        if(Read_Keypad ==1)
-                            state = RED_ON;
-                }
-
-                if(Read_Keypad() == 2){
-                    SysTick_Delay(1000000);
-                        if(Read_Keypad() ==2)
-                            state = GREEN_ON;
-                }
-                if(Read_Keypad() == 3){
-                    SysTick_Delay(1000000);
-                        if(Read_Keypad() ==3)
-                            state = BLUE_ON;
-                }
-                    break;
-
              case RED_ON:
-             while(1){
-                 num = Read_Keypad();
+                 i = 0;
+                 printf("\nRed - Enter 3 digit duty cycle\n");
+                 while(1){
+                    num = Read_Keypad();
 
-                        if ((num != 10) && (num < 13) && (num > 0)){                        //Continues if valid key pressed.
-                            if (num != 12){                                                     //If the pound key is not pressed
-                                if (i < 4){                                                         //the program stores the value in array.
-                                    code[i] = num;                                              //If more that 4 digits are entered,
-                                }                                                                   //the digits are shifted over
-                                if (i > 3){                                                         //and the new value is stored.
-                                    code[0] = code[1];
-                                    code[1] = code[2];
-                                    code[2] = code[3];
-                                    code[3] = num;
-                                    i = 3;
-                                }
-                                i++;
-                            }
+                    if ((num != 10) && (num < 12) && (num > 0)){                        //Continues if valid key pressed.
+                        if (num == 11)
+                            num = 0;
+                        code[i] = num;             //the program stores the value in array.
+                        i++;
+                        if (i == 3)
+                            break;
                         }
-                        dutyR = arrayToDuty(code);
-                        TimerA0_PWM(dutyR, dutyG, dutyB);
-                        break;
-                }
+                    }
+
+                dutyR = arrayToDuty(code);
+                TimerA0_PWM(dutyR, dutyG, dutyB);
+                state = DEFAULT;
+                break;
 
              case GREEN_ON:
-             while(1){
-                 num = Read_Keypad();
+                 i = 0;
+                 printf("\nGreen - Enter 3 digit duty cycle\n");
+                 while(1){
+                    num = Read_Keypad();
 
-                        if ((num != 10) && (num < 13) && (num > 0)){                        //Continues if valid key pressed.
-                            if (num != 12){                                                     //If the pound key is not pressed
-                                if (i < 4){                                                         //the program stores the value in array.
-                                    code[i] = num;                                              //If more that 4 digits are entered,
-                                }                                                                   //the digits are shifted over
-                                if (i > 3){                                                         //and the new value is stored.
-                                    code[0] = code[1];
-                                    code[1] = code[2];
-                                    code[2] = code[3];
-                                    code[3] = num;
-                                    i = 3;
-                                }
-                                i++;
-                            }
+                    if ((num != 10) && (num < 12) && (num > 0)){                        //Continues if valid key pressed.
+                        if (num == 11)
+                            num = 0;
+                        code[i] = num;             //the program stores the value in array.
+                        i++;
+                        if (i == 3)
+                            break;
                         }
-                        dutyG = arrayToDuty(code);
-                        TimerA0_PWM(dutyR, dutyG, dutyB);
-                        break;
-                }
+                    }
+
+                dutyG = arrayToDuty(code);
+                TimerA0_PWM(dutyR, dutyG, dutyB);
+                state = DEFAULT;
+                break;
 
              case BLUE_ON:
-             while(1){
-                 num = Read_Keypad();
+                 i = 0;
+                 printf("\nBlue - Enter 3 digit duty cycle\n");
+                 while(1){
+                    num = Read_Keypad();
 
-                        if ((num != 10) && (num < 13) && (num > 0)){                        //Continues if valid key pressed.
-                            if (num != 12){                                                     //If the pound key is not pressed
-                                if (i < 4){                                                         //the program stores the value in array.
-                                    code[i] = num;                                              //If more that 4 digits are entered,
-                                }                                                                   //the digits are shifted over
-                                if (i > 3){                                                         //and the new value is stored.
-                                    code[0] = code[1];
-                                    code[1] = code[2];
-                                    code[2] = code[3];
-                                    code[3] = num;
-                                    i = 3;
-                                }
-                                i++;
-                            }
-                        }
-                        dutyB = arrayToDuty(code);
-                        TimerA0_PWM(dutyR, dutyG, dutyB);
-                        break;
-                       }
-                }
-        }       
-}
+                    if ((num != 10) && (num < 12) && (num > 0)){                        //Continues if valid key pressed.
+                        if (num == 11)
+                            num = 0;
+                        code[i] = num;             //the program stores the value in array.
+                        i++;
+                        if (i == 3)
+                            break;
+                    }
+                 }
 
 
-void b1pressed(void){                                 //button debouncing function for button 1
-    //uint16_t x = 0;
-    if (!(P1->IN & BIT7)){
-        SysTick_Delay(15000);
-        if (!(P1->IN & BIT7)&&((dutyR != 0)||(dutyG != 0)||(dutyB != 0))) {
-            tempR=dutyR;    tempG=dutyG;    tempB=dutyB;
-            dutyR=0;    dutyG=0;    dutyB=0;
-            TimerA0_PWM(dutyR, dutyG, dutyB);
-        }
-        if (!(P1->IN & BIT7)&&((dutyR == 0)||(dutyG == 0)||(dutyB == 0))){
-            dutyR = tempR;  dutyG = tempG;   dutyB = tempB;
-            TimerA0_PWM(dutyR, dutyG, dutyB);
+                dutyB = arrayToDuty(code);
+                TimerA0_PWM(dutyR, dutyG, dutyB);
+                state = DEFAULT;
+                break;
+
+             case DEFAULT:
+                 printf("\nPress 1 for Red, 2 for Green, or 3 for blue\n");
+                 while (1) {
+                     num = Read_Keypad();
+                     if ((num != 10) && (num < 12) && (num > 0)){                        //Continues if valid key pressed.
+                         if (num == 11)
+                             num = 0;
+                         if(num == 1){
+                             state = RED_ON;
+                             break;
+                         }
+                         else if(num == 2){
+                             state = GREEN_ON;
+                             break;
+                         }
+                         else if(num == 3){
+                             state = BLUE_ON;
+                             break;
+                         }
+                         else
+                             continue;
+                     }
+                 }
+
+                 break;
+
+             default:
+                 state = DEFAULT;
         }
     }
 }
 
-void SysTick_Delay(int delayms) {                       //delay function using system clock
-    SysTick->LOAD = delayms-1;
-    SysTick->VAL = 0;
-    SysTick->CTRL |= BIT0;
-    while (!(SysTick->CTRL & BIT(16)));
-}
-
 void RGB_init(void){
-    P1->SEL0 &= ~(BIT7);      //GPIO
-    P1->SEL1 &= ~(BIT7);
-    P1->DIR &= ~(BIT7);       //set button to input
-    P1->REN |= (BIT7);        //enable resistor
-    P1->OUT |= (BIT7);        //use pull up resistor
+    P1-> SEL0 &=~ BIT7;
+    P1-> SEL1 &=~ BIT7;
+    P1-> DIR &=~ BIT7;
+    P1-> REN |= BIT7; //pullup resistor
+    P1-> OUT |= BIT7;
+    P1-> IE |= BIT7; //enable interupts for P1.1
+    P1-> IES |= BIT7; // Set pin to interrupt to trigger when it goes high->low
+    P1-> IFG &= ~BIT7; //clear P1 interrupt flags
 
-    P4->SEL0 &= ~(BIT4|BIT5|BIT6);      //GPIO
-    P4->SEL1 &= ~(BIT4|BIT5|BIT6);
-    P4->DIR |= (BIT4|BIT5|BIT6);        //Set LED pins to Outputs
-    P4->OUT |= (BIT4|BIT5|BIT6);       //Start with a low output
+    P2->SEL0 |= (BIT4|BIT5|BIT6);      //GPIO
+    P2->SEL1 &= ~(BIT4|BIT5|BIT6);
+    P2->DIR |= (BIT4|BIT5|BIT6);        //Set LED pins to Outputs
+    P2->OUT &= ~(BIT4|BIT5|BIT6);       //Start with a low output
+
+    P4->SEL0 &= ~(0x7F);                                                        //initialize all pins to GPIO
+    P4->SEL1 &= ~(0x7F);                                                        //as inputs with pull up resistors
+    P4->DIR &= ~(0x7F);
+    P4->REN |= 0x7F;
+    P4->OUT |= 0x7F;
+
 }
 
 void TimerA0_PWM(int duty1, int duty2, int duty3){               //runs timer given the specified duty cycle
     TIMER_A0-> CTL = 0b1000010100;                      //Count up using smclk, clears TAOR register, /1
-    TIMER_A0-> CCR[0] = 37500 - 1;                      //TimerA will count up to 63000-1
+    TIMER_A0-> CCR[0] = 50000 - 1;                      //TimerA will count up to 63000-1
 
     if (duty1 == 0)
         TIMER_A0-> CCR[1] = 0;
+    else
+        TIMER_A0-> CCR[1] = (50000 * duty1 / 100) - 1;
+
     if (duty2 ==0)
         TIMER_A0-> CCR[2] = 0;
+    else
+        TIMER_A0-> CCR[2] = (50000 * duty2 / 100) - 1;
+
     if (duty3 == 0)
         TIMER_A0-> CCR[3] = 0;
+    else
+        TIMER_A0-> CCR[3] = (50000 * duty3 / 100) - 1;
 
-
-    else {
-        TIMER_A0-> CCR[1] = (37500 * duty1 / 100) - 1;
-        TIMER_A0-> CCR[2] = (37500 * duty2 / 100) - 1;
-        TIMER_A0-> CCR[3] = (37500 * duty3 / 100) - 1;
-    }
     TIMER_A0-> CCTL[1] = 0b11100000;
     TIMER_A0-> CCTL[2] = 0b11100000;                    //reset/set mode
     TIMER_A0-> CCTL[3] = 0b11100000;
@@ -203,7 +180,6 @@ int Read_Keypad (void){                                                         
     for (i = 4; i < 7; i++){
         P4->DIR |= (0x01 << i);
         P4->OUT &= ~(0x01 << i);
-        __delay_cycles(5000);
         for (j = 0; j < 4; j++){
             if (!(P4->IN & BIT(j))){
                 while (!(P4->IN & BIT(j)));
@@ -229,4 +205,22 @@ int arrayToDuty(int code[]){
     newDuty = (code[0] * 100) + (code[1] * 10) + code[2];
 
     return newDuty;
+}
+
+void PORT1_IRQHandler(void){
+    if(P1->IFG & BIT7){
+        while(!(P1->IN & BIT7));
+        if (dutyR + dutyG + dutyB){
+            printf("Disabled\n");
+            tempR=dutyR;    tempG=dutyG;    tempB=dutyB;
+            dutyR=0;    dutyG=0;    dutyB=0;
+            TimerA0_PWM(0,0,0);
+        }
+        else{
+            printf("Enabled\n");
+            dutyR = tempR;  dutyG=tempG;    dutyB=tempB;
+            TimerA0_PWM(dutyR, dutyG, dutyB);
+        }
+    P1->IFG = 0;
+    }
 }
